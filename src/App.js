63c16e87f524da0791a7789c7c8d225298ceee3f
Ondairos <ondairos@gmail.com>
2023-02-08 15:53:55 +0200

    refactored Notification with Material UI style

--- src/App.js
+++ src/App.js
@@ -105,15 +105,17 @@ const Login = (props) => {
 // other comps
 const About = () => (
   <div>
-    <h2>About anecdote app</h2>
-    <p>According to Wikipedia:</p>
+    <Container>
+      <h2>About anecdote app</h2>
+      <p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+      <em>An anecdote is a brief, revealing account of an individual person or an incident.
+        Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
+        such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
+        An anecdote is "a story with a point."</em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+      <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+    </Container>
   </div>
 )
 
@@ -130,7 +132,7 @@ const Notification = ({ message }) => {
 
   if (message) {
     notification = (
-      <Alert variant='success'>
+      <Alert severity='success'>
         {message}
       </Alert >
     )
